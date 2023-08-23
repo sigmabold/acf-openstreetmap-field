7fb49ce4c405917e68198503aa9c9f3a2ac1a2ff
Kris Reeves <krisre@sigmabold.com>
2023-08-23 10:14:33 -0700

    Build before bundle

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,5 +6,12 @@ DIR="$( cd -P "$( dirname "$0" )" && pwd )"
 PLUGIN="$( basename $DIR )"
 VER="$( jq -r .version package.json )"
 PREFIX="$PLUGIN.$VER"
+
+npx gulp build
+
+if [[ -n $(git status -s) ]]; then
+    echo "Git is dirty, refusing to build bundle"
+    exit 1
+fi
 
 git archive --format=zip --prefix="$PLUGIN/" -o "$PREFIX.zip" HEAD
